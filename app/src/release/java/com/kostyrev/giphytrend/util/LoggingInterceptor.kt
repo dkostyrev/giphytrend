bdcc7a4cf2cd651c1e804c4ad8388a5d9af6f09f
Dmitry Kostyrev <183590+dkostyrev@users.noreply.github.com>
2018-01-24 19:43:08 +0300

    Fix release logging interceptor

--- app/src/release/java/com/kostyrev/giphytrend/util/LoggingInterceptor.kt
+++ app/src/release/java/com/kostyrev/giphytrend/util/LoggingInterceptor.kt
@@ -1,12 +1,11 @@
 package com.kostyrev.giphytrend.util
 
-import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class LoggingInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        return chain.proceed(chain.request)
+        return chain.proceed(chain.request())
     }
 }
